7d5928bf15439b40c399951bb661f730f0a4ad6a
AlexanderV <avaughan@barefootsolutions.com>
2017-12-14 15:13:30 -0800

    Add server and servers components

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'; //allows type decorator of Component
+
+
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html', //set html template of this component in current directory
+}) 
+export class ServerComponent {
+//Component is essentially a TypeScript class
+
+}
